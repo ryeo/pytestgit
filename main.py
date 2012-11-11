f58552be8a05922372053c79fd41aa7dede9a478
ryeo <robertyeo@hotmail.com>
2012-11-10 22:25:38 -0800

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from google.appengine.ext import webapp
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        self.response.out.write('Helldddddo world!')
 
 
 app = webapp.WSGIApplication([('/', MainHandler)],
