0128f0067178e509c8939d2ca0a01241629225db
ryeo <robertyeo@hotmail.com>
2012-11-12 07:59:53 -0800

    Team 2 Update 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from google.appengine.ext import webapp
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Feature world! Master Update 1')
+        self.response.out.write('Feature world! Master Update 1 by Team 2')
 
 
 app = webapp.WSGIApplication([('/', MainHandler)],
